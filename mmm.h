296852627d9ae74e929da1f815042ff000ab8518
bgamble01 <73133168+bgamble01@users.noreply.github.com>
2022-03-22 23:06:55 -0700

    Update mmm.h

--- mmm.h
+++ mmm.h
@@ -4,6 +4,16 @@
 // globals (anything here would be shared with all threads) */
 // I would declare the pointers to the matrices here (i.e., double **A, **B, **C),
 // as well as the size of the matrices, etc.
+double **A;
+double **B;
+double **C;
+double **D;
+int size;
+
+struct parParams{
+    int s_row;
+    int f_row;
+};
 
 void mmm_init();
 void mmm_reset(double **);
@@ -11,5 +21,7 @@ void mmm_freeup();
 void mmm_seq();
 void *mmm_par(void *args);
 double mmm_verify();
+void printMatrix(double **);
 
 #endif /* MMM_H_ */
+
